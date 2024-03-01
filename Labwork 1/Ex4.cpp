945022b5549ed839d40deef09a5c987584c2ad30
nhu220840 <nhu220840@gmail.com>
2024-03-01 14:17:07 +0700

    add const number PI in ex4 in labwork 1

--- Labwork 1/Ex4.cpp
+++ Labwork 1/Ex4.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
+const double PI = 3.14;
+
 int main(){
     int given_radius = 4;
-    double area1 = given_radius * given_radius * 3.14;
-    double circumference1 = 2 * given_radius * 3.14;
+    double area1 = given_radius * given_radius * PI;
+    double circumference1 = 2 * given_radius * PI;
     printf("An area of a circle with given radius equal 4 is: %lf\n", area1);
     printf("A circumference of a circle with given radius equal 4 is: %lf\n", circumference1);
 
     printf("Please enter a radius: ");
     int radius; scanf("%d", &radius);
-    double area2 = radius * radius * 3.14;
-    double circumference2 = 2 * radius * 3.14;
+    double area2 = radius * radius * PI;
+    double circumference2 = 2 * radius * PI;
     printf("An area of a circle is: %lf\n", area2);
     printf("A circumference of a circle is: %lf\n", circumference2);
 }
