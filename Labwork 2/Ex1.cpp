c9be4c714704ee96f36ccb49b0c9917229c39559
nhu220840 <nhu220840@gmail.com>
2024-03-04 11:09:37 +0700

    add cmt to clearly

--- Labwork 2/Ex1.cpp
+++ Labwork 2/Ex1.cpp
@@ -3,6 +3,7 @@
 int main(){
     printf("Please enter a temperature form Centigrade: ");
     double C; scanf("%lf", &C);
-    double F = C * 9 / 5 + 32;
+    double F = C * 9 / 5 + 32; //9/5 * C + 32 sai boi vi neu lay 9/5 ma ko ep kieu kqua se ra 1 so int 1.0 
+    //nen sau do nhan voi C + 32 se lam sai lech kqua
     printf("A temperature form Fahrenheit is: %lf", F);
 }
