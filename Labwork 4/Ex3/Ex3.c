68c3b2dffb8719368f480beb1271bd534eeb8250
nhu220840 <nhu220840@gmail.com>
2024-03-28 12:53:26 +0700

    fix logic grammar

--- Labwork 4/Ex3/Ex3.c
+++ Labwork 4/Ex3/Ex3.c
@@ -3,7 +3,7 @@
 #include "Gxy.h"
 
 int main(){
-    printf("Please enter 2 integer numbers x and y: ");
+    printf("Please enter 2 numbers x and y: ");
     double x, y; scanf("%lf %lf", &x, &y);
     printf("f(x) = %.2lf\ng(x, y) = %.2lf", fx(x), gxy(x, y));
 }
