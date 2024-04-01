5791c7bc8cb56fec9bedc980a3c836d27ec2a739
nhu220840 <nhu220840@gmail.com>
2024-04-01 20:22:12 +0700

    add

---
+++ 11.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    long long sum = 0;
+    for(int i = 1; i <= n; i++){
+        sum += (2 * i) - 1;
+    }
+    printf("%lld", sum);
+}
