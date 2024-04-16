688e26a66ee94904ba42aeb0d7c82d342b97c235
nhu220840 <nhu220840@gmail.com>
2024-04-16 14:18:21 +0700

    Add file ex4

--- 22BA13248_DoNguyenGiaNhu/Ex4.c
+++ 22BA13248_DoNguyenGiaNhu/Ex4.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
-#include "GCD.h"
+
+void bubbleSort(int a[], int n){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < i; j++){
+            if(a[j] > a[j + 1]){
+                int tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
+        }
+    }
+}
 
 int main(){
-    int a, b; scanf("%d %d", &a, &b);
-    printf("%d", gcd(a, b));    
+    int n; scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+    bubbleSort(a, n);
+    for(int i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
 }
--- 22BA13248_DoNguyenGiaNhu/GCD.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int gcd(int a, int b){
-    if(b == 0) return a;
-    return gcd(b, a % b);
-}
