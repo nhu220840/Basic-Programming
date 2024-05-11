cf35d55cadcb60f331f9e69c22c91b710c564beb
nhu220840 <nhu220840@gmail.com>
2024-05-11 19:02:56 +0700

    Add

--- testc2.c
+++ testc2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    printf("Enter a month: ");
-    int month; scanf("%d", &month);
-    if(month <= 0 || month > 12){
-        printf("Invalid month!");
+    int n; scanf("%d", &n);
+    float sum = 0;
+    for(int i = 1; i <= n; i++){
+        sum += (float)1/i; 
     }
-    else if(month == 1 || month == 3 || month)
+    printf("%f ", sum);
 }
