7d4c9ab8b4708c051031c40757c49555b2d8b0e0
nhu220840 <nhu220840@gmail.com>
2024-05-19 21:55:56 +0700

    correction ex3

--- Algorithm/Labwork 1/Ex3.c
+++ Algorithm/Labwork 1/Ex3.c
@@ -3,6 +3,11 @@
 int main(){
     printf("Enter 2 numbers: ");
     int a, b; scanf("%d %d", &a, &b);
+    if(a > b){
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
     printf("All odd numbers between %d and %d are: ", a, b);
     for(int i = a; i <= b; i++){
         if(i % 2 != 0){
