df646919f35ef62798a687f2c5a291c98744f9e7
nhu220840 <nhu220840@gmail.com>
2024-05-19 23:04:08 +0700

    correct ex4

--- Algorithm/Labwork 1/Ex4.c
+++ Algorithm/Labwork 1/Ex4.c
@@ -4,6 +4,8 @@
 int main(){
     printf("Enter an integer number: ");
     int n; scanf("%d", &n);
+    if(n <= 1)
+        printf("%d is not a prime number", n); return 0;
     for(int i = 2; i <= sqrt(n); i++){
         if(n % i == 0){
             printf("%d is not a prime number", n);
