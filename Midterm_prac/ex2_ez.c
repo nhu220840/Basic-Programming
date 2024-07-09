c1eef9f2a61c4c839c6095fe6bffd8634ef5076f
nhu220840 <nhu220840@gmail.com>
2024-07-09 20:10:03 +0700

    update

--- Midterm_prac/ex2_ez.c
+++ Midterm_prac/ex2_ez.c
@@ -4,6 +4,14 @@
 int main(){
     printf("Enter 3 numbers: ");
     int a, b, c; scanf("%d %d %d", &a, &b, &c);
-    printf("Minimum: %d\n", (int)fmin(a, fmin(b, c)));
-    printf("Maximunm: %d", (int)fmax(a, fmax(b, c)));
+    int min = a, max = a;
+    if(b > max) max = b;
+    if(c > max) max = c;
+
+    if(b < min) min = b;
+    if(c < min) min = c;
+    // printf("Minimum: %d\n", (int)fmin(a, fmin(b, c)));
+    // printf("Maximum: %d", (int)fmax(a, fmax(b, c)));
+    printf("Minimum: %d\n", min);
+    printf("Maximum: %d", max);
 }
