0ebdf578951e083710fff7cdd18cf4b504ca5290
nhu220840 <nhu220840@gmail.com>
2024-09-20 21:59:17 +0700

    update

--- Midterm_prac/sum_ez.c
+++ Midterm_prac/sum_ez.c
@@ -9,7 +9,7 @@ int main(){
     int n; scanf("%d", &n);
     float sum = 0;
     for(int i = 1; i <= n; i++){
-        sum += 1/i;
+        sum += 1.0/i;
     }
     printf("Sum = %f", sum);
 }
--- array.c
+++ array.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
 
-int dq(int n){
-    if(n != 0){
-        dq(n / 2);
-        printf("%d", n % 2);
-    }
-}
-
 int main(){
     int n; scanf("%d", &n);
-    int cnt = 0;
-    int rev[20];
-    while(n != 0){
-        rev[cnt] = n % 2;
-        cnt++;
-        n /= 2;
-    }
-    for(int i = cnt - 1; i >= 0; i--){
-        printf("%d", rev[i]);
+    for(int i = 1; i <= n; i++){
+        if(i % 2 != 0){
+            printf("%d ", i);
+        }
     }
 }
